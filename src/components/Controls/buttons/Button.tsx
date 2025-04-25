8fe0eeeef6e1708cb64725bc50388bc7dca8071a
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-04-24 17:05:51 -0700

    GenerateAdFlow fix on button

--- src/components/Controls/buttons/Button.tsx
+++ src/components/Controls/buttons/Button.tsx
@@ -11,7 +11,7 @@ import styles from './Button.module.css'
 
 const Button:FC<ButtonProps> = ({ title, icon, className='auto', width='100%' }) => {
   return (
-    <button className={`${styles.button} ${styles[className]}`} style={{ width: width}}>
+    <button className={`${styles.button} ${styles[className]}`} style={{ width: width }}>
         {title} {icon}
     </button>
   )
