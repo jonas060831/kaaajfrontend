01d2fee48a5d998a1ac08e4e92662fc763755b18
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-04-30 18:59:16 -0700

    add go live submit button

--- src/pages/protected/manager/components/GoLive.tsx
+++ src/pages/protected/manager/components/GoLive.tsx
@@ -1,10 +1,19 @@
 
+import Icon from '../../../../components/Icon/Icon'
 import styles from './pages.module.css'
 const GoLive = () => {
   return (
     <div className={styles.container}>
 
-        <button type="submit">GoLive</button>
+        <button
+         className={styles.live_button} type="submit"
+        >
+          <Icon category="GreenIndicator" width={20} height={20}/>
+          <span>
+             Go Live
+          </span>
+          <Icon category="SignIn" width={20} height={20}/>
+        </button>
     </div>
   )
 }
