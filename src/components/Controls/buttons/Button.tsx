ea49564f633a5c620125c577b868cc1d82048de0
jonaspaulosulit <jonas_sulit2001@yahoo.com>
2025-05-12 19:51:15 -0700

    disabling button whenever password and repeat password isnt a match

--- src/components/Controls/buttons/Button.tsx
+++ src/components/Controls/buttons/Button.tsx
@@ -7,18 +7,19 @@ type ButtonProps = {
     width?: string;
     type?: "button" | "submit" | "reset";
     isLoading?: boolean;
+    isDisabled?: boolean;
     onClick?: () => void;
 }
 
 import styles from './Button.module.css'
 import Icon from "../../Icon/Icon";
 
-const Button:FC<ButtonProps> = ({ title, icon, width='100%', type='button', isLoading, className='default', onClick }) => {
+const Button:FC<ButtonProps> = ({ title, icon, width='100%', type='button', isLoading, className='default', onClick, isDisabled }) => {
   return (
     <button
      className={`${styles.button} ${styles[className]} ${isLoading ? styles.loading : ''}`}
      type={type}
-     disabled={isLoading}
+     disabled={isLoading || isDisabled}
      onClick={onClick}
      style={{ width: width}}
     >
